6e31581d07331e9d667d55b3ac00d8679370a63a
Tiago Funk <tiagoff.tf@gmail.com>
2023-01-25 19:47:15 -0300

    Adição do contato de e-mail

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,7 +5,7 @@ export const About = () => {
   const TEXT = [
     "Tenho formação em Engenharia de Software e atualmente sou aluno de mestrado em computação aplicada.",
     "Já atuei com monitoria e projetos de pesquisa durante a faculdade e trabalhei com desenvolvimento android por um breve período.",
-    "Programo em C/C++, Python e Java.",
+    "Programo em Java, C/C++ e Python.",
   ];
 
   return (
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,13 +6,17 @@ import gmail from "../../assets/img/About/gmail.png";
 
 export const Contact = () => {
 
+  const githubLink = "https://github.com/tiagofunk";
+  const linkedinLink = "https://www.linkedin.com/in/tiago-funk/";
+  const emailLink = "mailto:tiagoff.tf@gmail.com?subject=Quero falar com você"
+
   return (
     <Container>
-        <Title>Meus contatos</Title>
+      <Title>Meus contatos</Title>
       <IconsArea>
-        <a href="https://github.com/tiagofunk" target="_blank" rel="noreferrer"><Icon src={github} /></a>
-        <a href="https://github.com/tiagofunk" target="_blank" rel="noreferrer"><Icon src={linkedin} /></a>
-        <Icon src={gmail} />
+        <a href={githubLink} target="_blank" rel="noreferrer"><Icon src={github} /></a>
+        <a href={linkedinLink} target="_blank" rel="noreferrer"><Icon src={linkedin} /></a>
+        <a href={emailLink} ><Icon src={gmail} /></a>
       </IconsArea>
     </Container>
   );
